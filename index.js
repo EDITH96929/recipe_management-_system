bcc74605104c2840b2329699bbe69f56a5d3206e
itsSunil001 <98368191+itsSunil001@users.noreply.github.com>
2025-01-01 09:40:12 +0530

    sunil

--- index.js
+++ index.js
@@ -156,3 +156,124 @@ recipe.reveal('.recipe-button', {
   });
   
   
+
+
+
+
+  //--------------------------------------------------------------
+
+  const apis=["7f2698d859f24c3f91102af507f7eae0","d346ae4a383543e59cf356a562969f53","8287c26d4cff4460b7e9473f9a076c5e","5a535224b9e0422a84515a0273df4df0"]
+const getKey=()=> Math.floor(Math.random()*apis.length)
+
+const apiKey = apis[getKey()]; 
+const recipeContainer = document.querySelector(".recipe-container");
+
+// const filterButtons = document.querySelectorAll(".filter");
+const searchBar = document.getElementById("search-bar");
+console.log(searchBar)
+const searchButton = document.getElementById("search-button");
+const toastContainer = document.getElementById("toast-container"); //toast message
+
+const reqConfig = {
+  method: "GET",
+  headers: {
+    "x-api-key": apiKey,
+  },
+};
+async function fetchRecipes(searchQuery = "") {
+  const endpoint = searchQuery
+    ? `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}`
+    : `https://api.spoonacular.com/recipes/random?number=18`;
+  const response = await fetch(endpoint, reqConfig);
+  const data = await response.json();
+console.log(data)
+  return searchQuery ? data.results : data.recipes;
+}
+
+
+function renderRecipes(recipes, category = "all") {
+  recipeContainer.innerHTML = "";
+ 
+
+  const filteredRecipes = recipes.filter((recipe) => {
+    if (category === "veg") return recipe.vegetarian;
+    if (category === "non-veg") return !recipe.vegetarian;
+    return true;
+  });
+
+  if (filteredRecipes.length === 0) {
+    const noResultMessage = document.createElement("div");
+    noResultMessage.classList.add("no-result");
+    noResultMessage.innerText =
+      "No recipes found. Please try a different search or filter.";
+    recipeContainer.appendChild(noResultMessage);
+    return;
+  }
+
+  filteredRecipes.forEach((recipe) => {
+    const card = document.createElement("div");
+    card.classList.add("recipe-card");
+
+    card.innerHTML = `
+    <!-- Circular Image -->
+    <img src="${recipe.image}" alt="${recipe.title}">
+
+    <!-- Recipe Details -->
+    <div class="recipe-details">
+      <h3 class="recipe-name">${recipe.title}</h3>
+    
+      <p class="ingredients">
+        Ready in ${recipe.readyInMinutes} minutes. Serves ${recipe.servings}.
+      </p>
+      <div class="icon-details">
+        <i class="fa-regular fa-clock"></i>
+        <i class="fa-regular fa-face-smile"></i>
+        <i class="fa-regular fa-user"></i>
+      </div>
+      <p class="details">${recipe.readyInMinutes} minutes | ${
+    recipe.vegetarian ? "Vegetarian" : "Non-Vegetarian"
+  } | ${recipe.servings} people</p>
+      <a href="recipe-details.html?id=${recipe.id}">
+        <button class="recipe-button" onclick="navigateToRecipe('${recipe.id}')">Explore</button>
+      </a>
+    </div>
+  `;
+    recipeContainer.appendChild(card);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(async function initialize() {
+  // if(!recipesFromStorage){
+
+      const recipes = await fetchRecipes();
+      renderRecipes(recipes);
+      // sessionStorage.setItem("currentRecipe", JSON.stringify(recipes));
+
+      
+  // }else{
+  //     renderRecipes(recipesFromStorage);
+
+  // }
+
+// filterButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const category = button.getAttribute("data-category");
+//     renderRecipes(recipes, category);
+//   });
+// });
+
+// searchButton.addEventListener("click", handleSearch);
+})();// start application
